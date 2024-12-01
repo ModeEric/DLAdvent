4240caa803bebe7530d1f4f6231d704668857867
ModeEric <ericjm4@illinois.edu>
2024-12-01 17:12:12 -0600

    Back prop but messy

--- day1/nn_from_scratch.py
+++ day1/nn_from_scratch.py
@@ -1,5 +1,4 @@
 #idea: Start with linear layers and simple activation, implement back prop, gradient descent
-#Work in progress, taking lunch break
 class Tensor1D():
     def __init__(self,width,init_method='zero'):
         self.width = width
@@ -51,37 +50,107 @@ class Tensor2D():
         return self.width
     def getHeight(self):
         return self.height
-
+    def __add__(self,B):
+        for i in range(len(B)):
+            for j in range(len(B.getData(i))):
+                self.setData(i,j,self.getData(i,j)+B.getData(i,j))
+    def __multiply__(self,scalar):
+        for i in range(len(self)):
+            for j in range(len(self.getData(i))):
+                self.setData(i,j,self.getData(i,j)*scalar)
+    def __subtract__(self,B):
+        self = self + B * -1
 
 
 
 class LinearLayer():
-    def __init__(self,n_channels,input_size,output_size,init_method):
-        self.n_channels = n_channels
+    def __init__(self,n_channels,input_size,output_size,init_method,copy_weights, copy_biases):
         self.input_size = input_size
         self.output_size = output_size
+        self.name="fclinear"
         if init_method == 'zero':
-            self.layers = [Tensor2D(input_size,output_size) for _ in range(n_channels)]
+            self.weights = Tensor2D(input_size,output_size)
+            self.biases = 0
+        elif init_method == "copy":
+            self.weights = copy_weights
+            self.biases = copy_biases
     def forward(self, x):
-        outputs = []
-        for layer in self.layers:
-            outputs.append(self.layers @ x)
+        outputs = self.weights @ x+self.biases
         return outputs
+
+class relu():
+    def __init__(self):
+        self.name = "relu"
+    def forward(x: Tensor2D):
+        for i in range(x.getHeight()):
+            for j in range(x.getWidth()):
+                elem = x.getData(i,j)
+                val = 0 if elem<0 else elem
+                x.setData(i,j,val)
+
+class Sequential():
+    def __init__(self,arr=[]):
+        self.layers = arr
+    def add_layer(self,layer):
+        self.layers.append(layer)
+    def forward(self,x):
+        tmp = x
+        for layer in self.layers:
+            tmp = layer(tmp)
+        return tmp
+
+def gradient_descent_backprop(model,inputs,labels,lr): # \nabla Cost w.r.t weights and biases
+    intermediates = [inputs]
+    tmp = inputs
+    for layer in model.layers:
+        tmp = layer(inputs)
+        intermediates.append(tmp)
+    del intermediates[-1]
+    outputs = tmp
+    error = (outputs-labels)
+    updates = []
+    for intermediate,layer in reversed(zip(intermediates,model.layers)):
+        if layer.name=="fclinear":
+            error_m = intermediate @ error.transpose()
+            error_b = error
+            updates.append({
+                "fclinear": {
+                    error_m:error_m,
+                    error_b:error_b  }}) #Struct?
+        elif layer.name=="relu":
+            errors = []
+            for i in range(len(intermediate)):
+                if intermediate[i]<0:
+                    errors.append(0)
+                else:
+                    errors.append(error[i])
+            updates.append({"relu": {
+                            errors }})
+    newmodel = Sequential()
+    for layer,update in zip(model.layers,updates):
+        if layer.name=="fclinear":
+            newmodel.add_layer(LinearLayer(layer.weights - lr * update["fclinear"].error_m,layer.biases-lr*update["fclinear"].error_b))
+        else:
+            newmodel.add_layer(relu)
+    return outputs,errors,newmodel
+        
+
     
-def relu(x: Tensor2D):
-    for i in range(x.getHeight()):
-        for j in range(x.getWidth()):
-            elem = x.getData(i,j)
-            val = 0 if elem<0 else elem
-            x.setData(i,j,val)
 
+        
 
+            
+
+    
 
 def gradient_descent(model,data,labels, lr,epochs):
-    for _ in range(epochs):
+    for i in range(epochs):
+        print(f"Starting epoch {i}")
         for k in range(len(data)):
-            outputs = model(data[k])
-            error = outputs - labels
-            model = model - lr * error
+            outputs, errors, model = gradient_descent_backprop(model,data[k],labels[k],lr)
+            print(outputs)
+            print(errors)
+    print("DONE")
 
 
+#Going to gym, will come back and clean up
