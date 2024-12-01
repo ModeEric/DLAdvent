19c0e43ee8c76637a69928ed22a9da63f61d5fce
ModeEric <ericjm4@illinois.edu>
2024-12-01 10:29:48 -0500

    Improving nn from scratch

--- day1/nn_from_scratch.py
+++ day1/nn_from_scratch.py
@@ -1,19 +1,59 @@
 #idea: Start with linear layers and simple activation, implement back prop, gradient descent
 #Work in progress, taking lunch break
 class Tensor1D():
-    def __init__(self,width,init_method):
+    def __init__(self,width,init_method='zero'):
         self.width = width
+        self.init_method = init_method
         if self.init_method=='zero':
             self.data = [0 for i in range(width)]
+    def __multiply__(self,B):
+        return sum([self.getData(i)*B.getData(i) for i in range(len(self))])
+    def __len__(self):
+        return self.width
+    def getData(self,i):
+        return self.data[i]
+    def setData(self,i,val):
+        self.data[i] = val
 
 class Tensor2D():
-    def __init__(self,width,height,init_method):
+    def __init__(self,width,height,init_method='zero'):
+        self.init_method = init_method
         self.width = width
         self.height = height
         if init_method == 'zero':
-            self.data = [Tensor1D(height) for j in range(width)]
+            self.data = [Tensor1D(width) for j in range(height)]
+    def transpose(self):
+        newtensordata = []
+        for j in range(self.width):
+            tmp = Tensor1D(self.height)
+            for i in range(self.height):
+                tmp.setData(i,self.data[i][j])
+            newtensordata.append(tmp)
+        self.data = newtensordata
+        tmp = self.width
+        self.width = self.height
+        self.height = tmp
     def __matmul__(self,B):
-        return [self.data[i][j]*]
+        return [[sum(self.getData(i)*B.transpose().getData(j)) for j in range(len(B.tranpose()))] for i in range(len(self))]
+    def __len__(self):
+        return self.height
+    def getData(self,i,j):
+        if j is None:
+            return self.data[i]
+        else:
+            return self.data[i][j]
+    def setData(self,i,j,val):
+        if j is None:
+            self.data[i] = val
+        else:
+            self.data[i][j] = val
+    def getWidth(self):
+        return self.width
+    def getHeight(self):
+        return self.height
+
+
+
 
 class LinearLayer():
     def __init__(self,n_channels,input_size,output_size,init_method):
@@ -25,3 +65,23 @@ class LinearLayer():
     def forward(self, x):
         outputs = []
         for layer in self.layers:
+            outputs.append(self.layers @ x)
+        return outputs
+    
+def relu(x: Tensor2D):
+    for i in range(x.getHeight()):
+        for j in range(x.getWidth()):
+            elem = x.getData(i,j)
+            val = 0 if elem<0 else elem
+            x.setData(i,j,val)
+
+
+
+def gradient_descent(model,data,labels, lr,epochs):
+    for _ in range(epochs):
+        for k in range(len(data)):
+            outputs = model(data[k])
+            error = outputs - labels
+            model = model - lr * error
+
+
